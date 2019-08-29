d289f9c98200ef1d2903000446c25250d9eb2363
lesteven <stevenle2011@gmail.com>
2019-08-28 20:32:16 -0700

    refactor: useForm hook

--- src/client/custHooks/useForm.js
+++ src/client/custHooks/useForm.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const useForm = (initialVal = '') => {
   const [value, onChange] = useState(initialVal);
   const formChange = (e) => onChange(e.target.value);
-  return [value, formChange];
+  return [value, formChange, onChange];
 }
 
 export default useForm;
