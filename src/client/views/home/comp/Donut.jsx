9f5ff95b03fa7f41a353d40a2e9d118d66923bef
lesteven <stevenle2011@gmail.com>
2019-08-28 23:45:33 -0700

    refactor: donut chart code

--- src/client/views/home/comp/Donut.jsx
+++ src/client/views/home/comp/Donut.jsx
@@ -4,33 +4,28 @@ const backgroundColor = ['darkmagenta','darkred','darkorange',
   'indigo', 'teal', 'tomato', 'olivedrab'];
 
 const Donut = ({ currMonth }) => {
-  const keys = currMonth? Object.values(currMonth.expenditure) : [];
-  const name = currMonth? `${currMonth.month}_${currMonth.year}` : '';
+  const values = Object.values(currMonth.expenditure);
+  const name = `${currMonth.month}_${currMonth.year}`;
   useEffect(() => {
-    if (currMonth) {
-      const keys = Object.keys(currMonth.expenditure);
-      const data = [];
-      for (let i = 0; i < keys.length; i++) {
-        data[i] = currMonth.expenditure[keys[i]];
-      }
-      const canvas = document.getElementById(name);
-      const ctx = canvas.getContext('2d'); 
-      const chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'doughnut',
-
-        // The data for our dataset
-        data: {
-          labels: Object.keys(currMonth.expenditure),
-          datasets: [{
-            backgroundColor,
-            data,
-          }]
-        },
-      })
-      return () => chart.destroy();
+    const keys = Object.keys(currMonth.expenditure);
+    const data = [];
+    for (let i = 0; i < keys.length; i++) {
+      data[i] = currMonth.expenditure[keys[i]];
     }
-  }, [...keys, currMonth])
+    const canvas = document.getElementById(name);
+    const ctx = canvas.getContext('2d'); 
+    const chart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: Object.keys(currMonth.expenditure),
+        datasets: [{
+          backgroundColor,
+          data,
+        }]
+      },
+    })
+    return () => chart.destroy();
+  }, [values, currMonth])
   return (
     <canvas id ={ name } ></canvas>
   )
