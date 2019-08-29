d66947fcf122113b864201ead943e00752de1cd1
lesteven <stevenle2011@gmail.com>
2019-08-28 18:14:32 -0700

    feat: add to data reducer

--- src/client/redux/data.js
+++ src/client/redux/data.js
@@ -1,5 +1,20 @@
+const add = 'ADD';
+const insert = 'INSERT';
 const reset = 'RESET';
 
+export const update = (payload) => {
+  return {
+    type: add,
+    payload
+  }
+}
+
+export const insertData = (payload) => {
+  return {
+    type: insert,
+    payload
+  }
+}
 
 export const clear = () => {
   return {
@@ -11,6 +26,10 @@ const initialState = [];
 
 export const data = (state = initialState, action) => {
   switch(action.type) {
+    case insert:
+      return action.payload;
+    case add:
+      return action.payload;
     case reset:
       return initialState;
     default:
