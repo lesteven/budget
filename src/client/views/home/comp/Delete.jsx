f79c0e6577618a9f7fd77f495b18c544f4057af2
lesteven <stevenle2011@gmail.com>
2019-08-29 16:57:25 -0700

    feat: add pop up before delete

--- src/client/views/home/comp/Delete.jsx
+++ src/client/views/home/comp/Delete.jsx
@@ -1,20 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import css from '../css/delete.css';
 import { update } from '../../../redux/data';
 import { useDispatch } from 'react-redux';
 
+const PopUp = ({ deleteData, cancel }) => {
+  const [yes, clickYes] = useState(false);
+  const [no, clickNo] = useState(false);
+  if (yes) {
+    deleteData();
+    cancel();
+  }
+  if (no) {
+    cancel(); 
+  }
+  return (
+    <div className = 'popUp'>
+      <div className= 'popUpInside'>
+      <p> Are you sure? </p>
+      <div className = 'popButtons'>
+        <button onClick = { () => clickYes(true) }> Yes </button>
+        <button onClick = { () => clickNo(true) }> No </button>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+
+
 const Delete = ({ currMonth, data }) => {
   const dispatch = useDispatch();
-  const onClick = () => {
+
+  const deleteData = () => {
     const copy = {...data};
     const name =`${currMonth.month}_${currMonth.year}`;
     delete copy[name];
     localStorage.setItem('history', JSON.stringify(copy));
     dispatch(update(copy));
   }
+
+  const [clicked, change] = useState(false);
+  const cancel = () => change(false);
+
   return (
-    <button className = 'deleteHist' onClick = { onClick}> 
+    <div className = 'delete-wrapper'>
+      { clicked? <PopUp 
+          deleteData = { deleteData }
+          cancel = { cancel }
+          /> : null }
+    <button className = 'deleteHist' onClick = { () => change(true) }> 
       Delete Month </button>
+    </div>
   )
 }
 
