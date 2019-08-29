cf7acdc392a82a0e62839670210126670b020dde
lesteven <stevenle2011@gmail.com>
2019-08-29 14:56:09 -0700

    feat: update about page

--- src/client/views/about/About.jsx
+++ src/client/views/about/About.jsx
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import css from './about.css';
 
 const About = () => {
+  const link = 'https://github.com/lesteven/budget';
   return (
     <div className='about'>
-      <p> Hello About! </p>
+      <p> A simple front end budgeting app. Only a demo! </p>
+      <p> Visit my 
+        <a href = { link } className='ghLink'> github </a>
+         page to see the code!
+      </p>
     </div>
   )
 }
