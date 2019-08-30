a1a67568069029e5c5e4f12a2dd103966b6d1398
lesteven <stevenle2011@gmail.com>
2019-08-29 17:08:30 -0700

    chore: remove redundant files

--- src/client/views/home/comp/data.js
+++ src/client/views/home/comp/data.js
@@ -1,8 +1,29 @@
 
-export const months = ["January", "February", "March", "April", "May", 
-  "June", "July", "August", "September", "October", "November", 
-  "December"];
-export const years = [2019,2020,2021,2022,2023,2024,2025];
+export const months = [
+  "January", 
+  "February", 
+  "March", 
+  "April", 
+  "May", 
+  "June", 
+  "July", 
+  "August", 
+  "September", 
+  "October", 
+  "November", 
+  "December"
+];
+
+export const years = [
+  2019,
+  2020,
+  2021,
+  2022,
+  2023,
+  2024,
+  2025
+];
+
 export const expenditure = {
   'Food': 0, 
   'Rent': 0, 
